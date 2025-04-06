889700384f147af7205ccb8d6b25eb56af3af0a1
ArmandCarreras <aclcodesolutions@gmail.com>
2025-04-06 20:46:08 +0200

    ADDED BACKBUTTON CONTROL

--- 3CO/src/app/app.component.ts
+++ 3CO/src/app/app.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ThemeService } from './shared/services/theme.service';
 import { StorageService } from './shared/services/storage.service';
 import { InitializeAppService } from './shared/services/SQLite/initialize.app.service';
-
+import { Platform, ToastController } from '@ionic/angular';
+import { Location } from '@angular/common';
+import { App } from '@capacitor/app';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   
-  constructor(private themeServ: ThemeService,
+
+  private lastBackPress = 0;
+  private timePeriodToExit = 2000; 
+
+  private subscription!: Subscription;
+
+  
+  constructor(
+    private themeServ: ThemeService,
     private storageServ: StorageService,
-    private initStorage: InitializeAppService
-  ) {   }
+    private initStorage: InitializeAppService,
+    private platform: Platform,
+    private location: Location,
+    private router: Router,
+    private toastController: ToastController
+  ) {
+    this.initializeApp();
+  }
+  
+  
+  
   
   async ngOnInit(){
     
@@ -24,6 +45,54 @@ export class AppComponent implements OnInit {
     await this.themeServ.initTheme();
   }
 
+
+  ngOnDestroy(): void {
+    this.subscription.closed ?? this.subscription.unsubscribe();
+  }
+
+
+
+
+
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.handleBackButton();
+    });
+  }
+
+  handleBackButton() {
+    this.subscription = this.platform.backButton.subscribeWithPriority(9999, async () => {
+      const currentUrl = this.router.url.split('?')[0]; // Strip query params
+      const rootTabRoutes = ['/tabs/labels', '/tabs/product', '/tabs/learn', 'tabs/account']; // <- Update these!
+
+      // CASE 1: If there's navigation history → go back
+      if (window.history.length > 1 && !rootTabRoutes.includes(currentUrl)) {
+        this.location.back();
+        return;
+      } 
+      else {
+        const currentTime = new Date().getTime();
+        
+        if (currentTime - this.lastBackPress < this.timePeriodToExit) {
+          console.log('Exiting app...');
+          App.minimizeApp();
+        } else {
+          this.lastBackPress = currentTime;
+          const toast = await this.toastController.create({
+            message: 'Press back again to exit',
+            duration: 2000,
+            position: 'top',
+            cssClass: 'backtoast'
+          });
+          await toast.present();
+        }
+      }
+    });
+  }
+  
+  
+  
 
   
 
